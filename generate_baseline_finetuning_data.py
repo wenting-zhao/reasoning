2d31b539aae8649c7326d10ec51eee5e184348df
Wenting Zhao <wentingzhao@outlook.com>
2024-04-29 16:24:33 -0400

    update baseline related code

--- generate_baseline_finetuning_data.py
+++ generate_baseline_finetuning_data.py
@@ -25,23 +25,22 @@ def extract_substrings(text):
 
     return matches[0]
 
-split = sys.argv[3]
-ds = load_dataset(sys.argv[1], split=split)
-option = sys.argv[2]
+name = sys.argv[1]
+split = sys.argv[2]
+option = sys.argv[3]
+ds = load_dataset(name, split=split)
 outs = []
 for one in ds:
-    curr = one['problem']
-    if split == "train":
-        if option == "answer":
-            answer = extract_substrings(one['solution'])
-            if answer is None:
-                continue
-            curr += '\\boxed{'+answer+'}'
-        elif option == "cot":
-            curr += one['solution']
-    outs.append({"text": curr})
-out_name = f"data/math/{sys.argv[1].split('/')[-1]}-{option}.json"
-if split=="test":
-    out_name = out_name.replace('.json', '-test.json')
+    curr = [{"role": "user", "content": "Solve the following math problem.\nPlease highlight your solution with \\boxed{number} where number is the numerical answer without unit.\n\n" +  one['problem']}]
+    if option == "answer":
+        answer = extract_substrings(one['solution'])
+        if answer is None:
+            continue
+        curr.append({"role": "assistant", "content": '\\boxed{'+answer+'}'})
+    elif option == "cot":
+        curr.append({"role": "assistant", "content": one['solution']})
+    curr = {"text": curr}
+    outs.append(curr)
+out_name = f"data/math/{sys.argv[1].split('/')[-1]}-{split}-{option}.json"
 out_ds = Dataset.from_list(outs)
 out_ds.to_json(out_name)
--- inference.py
+++ inference.py
@@ -42,14 +42,17 @@ def main():
     parser.add_argument("--temperature", type=float, default=1.0, help="temperature for sampling")
     parser.add_argument("--batch_size", "-b", type=int, default=32, help="batch size")
     parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument("--num_samples", type=int, default=1, help="how many samples to generate")
     parser.add_argument("--port", type=str, default="30000", help="port number")
+    parser.add_argument("--start_server", action="store_true", help="whether to start sglang server")
     args = parser.parse_args()
 
     set_seed(args)
 
     ds = load_dataset('json', data_files=args.dataset_name, split='train')
 
-    pro = start_server(args.model_name, args.port)
+    if args.start_server:
+        pro = start_server(args.model_name, args.port)
     sgl.set_default_backend(RuntimeEndpoint(f"http://localhost:{args.port}"))
 
     logger.info(args)
@@ -58,17 +61,19 @@ def main():
 
     for i in tqdm(range(0, len(ds), args.batch_size)):
         data = ds.select(range(i, min(i+args.batch_size, len(ds))))
-        prompt_text = [one["text"] for one in data]
+        prompt_text = [one["text"][:-1] for one in data]
 
-        results = sample_completion(prompt_text, temperature=args.temperature, max_tokens=args.max_new_tokens, samples=1)
+        results = sample_completion(prompt_text, temperature=args.temperature, max_tokens=args.max_new_tokens, samples=args.num_samples)
         generated_sequences += results
 
+    generated_sequences = [generated_sequences[i:i+args.num_samples] for i in range(0, len(generated_sequences), args.num_samples)]
     ds = ds.add_column(name="output", column=generated_sequences)
-    dataset_name = args.dataset_name.split('/')[-1]
+    dataset_name = args.dataset_name.split('/')[-1].replace(".json", "")
     model_name = args.model_name.split('/')[-1]
-    out_name = f"out/baselines/{dataset_name}-{model_name}-temp-{args.temperature}.json"
+    out_name = f"out/inference/{dataset_name}-{model_name}-num{args.num_samples}.json"
     ds.to_json(out_name)
-    os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
+    if args.start_server:
+        os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
 
 
 if __name__ == "__main__":
