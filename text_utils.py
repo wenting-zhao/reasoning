3eebae605ecd9e23eecd91f48c1abfc32abda620
Wenting Zhao <wentingzhao@outlook.com>
2024-04-29 22:42:57 -0400

    Added utils code.

---
+++ text_utils.py
@@ -0,0 +1,22 @@
+def format_qa(text, eos_token="<|eot_id|>"):
+    text = text.split("assistant")[-1]
+    start_idx = text.find("Sub-problem")
+    end_idx = text.rfind(eos_token)
+    text = text[start_idx:end_idx]
+    curr = []
+    while True:
+        q_idx = text.find('Sub-problem')
+        a_idx = text.find('Solution')
+        q = text[q_idx:a_idx]
+        text = text[a_idx:]
+        if 'Sub-problem' not in text:
+            a = text
+            curr.append([q.replace('Sub-problem:', '').strip(), a.replace('Solution to the sub-problem:', '').strip()])
+            break
+        else:
+            q_idx = text.find('Sub-problem')
+            a = text[:q_idx]
+            text = text[q_idx:]
+            curr.append([q.replace('Sub-problem:', '').strip(), a.replace('Solution to the sub-problem:', '').strip()])
+    return curr
+
