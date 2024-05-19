fde7158c7e2c6cac128179852f107561629ee27d
Wenting Zhao <wentingzhao@outlook.com>
2024-05-19 11:37:15 -0400

    updates

--- batch_run.sh
+++
@@ -1,4 +0,0 @@
-for i in {0..7499..500}
-    do
-        python sample-model-a.py --dataset_name hendrycks/competition_math --dataset_split train --model meta-llama/Meta-Llama-3-8B --num_samples 30 --start $i --end $((${i}+500))
-    done
