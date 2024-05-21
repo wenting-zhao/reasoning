7d0422c8cdf3ea52b7ac4421f6ce7b67015ab839
Wenting Zhao <wentingzhao@outlook.com>
2024-05-21 10:38:19 -0400

    small fix

--- run.sh
+++ run.sh
@@ -3,16 +3,17 @@ data=$2
 name=$3
 lr=$4
 bs=$5
+gs=2
 
 python run_clm_no_trainer.py \
     --model_name_or_path $model \
     --train_file $data \
     --validation_file data/math/competition_math-test-cot.json \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size $bs \
     --num_train_epochs 1 \
-    --gradient_accumulation_steps $bs \
+    --gradient_accumulation_steps $gs \
     --learning_rate $lr \
-    --output_dir models/$name-lr$lr-bs$bs \
+    --output_dir models/$name-lr$lr-bs$((bs*gs)) \
     --with_tracking \
     --report_to wandb \
     --low_cpu_mem_usage
