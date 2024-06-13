d0e5605cfaa174f30b1ec931a148a52dfaa330ab
Wenting Zhao <wentingzhao@outlook.com>
2024-06-12 23:11:28 -0400

    fixed data gen

--- generate_star_finetuning_data.py
+++ generate_star_finetuning_data.py
@@ -10,19 +10,25 @@ def main():
     test_examples = load_dataset('json', data_files=sys.argv[2:], split="train")
     outs = []
     count = []
+    limit = 1
     for example in tqdm(test_examples):
         count.append(0)
-        for one in example['output']:
+        key = 'output' if 'output' in example else 'star'
+        #for one in example['output']:
+        #for one in example['star']:
+        tot = 0
+        for one in example[key]:
+            if tot >= limit: break
             equiv = check(one, example['solution'])
             if equiv:
+                tot += 1
                 count[-1] = 1
                 outs.append({"text": [{"role": "user", "content": "Solve the following math problem.\nPlease highlight your solution with \\boxed{number} where number is the numerical answer without unit.\n\n" +  example['problem']}, {"role": "assistant", "content": format_cot(one)}]})
-                break
 
     print("correct chains per example:", len(outs)/len(test_examples))
     print("examples that were correct:", sum(count))
     test_examples = Dataset.from_list(outs)
-    test_examples.to_json(f"data/math/star_iteration{sys.argv[1]}.json")
+    test_examples.to_json(f"data/math/star_iteration{sys.argv[1]}_max{limit}.json")
 
 if __name__ == '__main__':
     main()
