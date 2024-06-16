78bfc4e364cbd79c877c08a6fe37ee48f40f07c2
Justin Chiu <justinchiu@g.harvard.edu>
2024-06-16 01:45:28 -0400

    fix example 3

--- examples/apps/train/3/code.py
+++ examples/apps/train/3/code.py
@@ -1,19 +1,21 @@
 def parse_test_cases():
-    num_cases = input()
+    num_cases = int(input())
     cases = []
-    for n in num_cases:
+    for n in range(num_cases):
         n, k = map(int, input().split())
         barrels = list(map(int, input().split()))
         cases.append((n, k, barrels))
-    return cases
+    return num_cases, cases
 
 
 def max_possible_difference(n, k, barrels):
     sorted_barrels = list(sorted(barrels, reverse=True))
-    return sum(sorted_barrels[:k])
+    return sum(sorted_barrels[:k+1])
 
 
 def main():
-    num_test_cases, test_cases = parse_test_cases
+    num_test_cases, test_cases = parse_test_cases()
     for test_case in test_cases:
         print(max_possible_difference(*test_case))
+
+main()
