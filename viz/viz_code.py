a3712e442462bdb03eed11392ce055124ab12a84
justinchiu-cohere <justinchiu@cohere.com>
2024-06-28 18:13:32 -0400

    code viz

--- viz/viz_code.py
+++ viz/viz_code.py
@@ -6,7 +6,7 @@ st.write("# Visualize code")
 
 dataset = datasets.load_dataset(
     "json",
-    data_files="out/model-a-samples-apps-train-gpt-4o-nofsFalse-num8-start0-end128.json",
+    data_files="out/model-a-samples-apps-train-gpt-4o-nofsFalse-num8-start0-end128-eval.json",
 )
 
 split = st.radio("split", options=dataset.keys())
@@ -19,21 +19,22 @@ st.write("## Difficulty:", example["difficulty"])
 
 st.write("## Question")
 st.write(example["question"].replace("\n", "  \n"))
-st.text(example["question"])
+#st.text(example["question"])
 
 st.write("## Attempts with correct answers")
 st.write(example["is_correct"])
 
 st.write("# Visualize attempt number")
-attempt_idx = st.number_input("attempt number", min_value=0, max_value=len(example["plan"]))
+attempt_idx = st.number_input("attempt number", min_value=0, max_value=len(example["code"]))
 
-st.write("## Plan")
-st.write(example["plan"][attempt_idx])
-st.text(example["plan"][attempt_idx])
+if "plan" in example:
+    st.write("## Plan")
+    st.write(example["plan"][attempt_idx])
+    #st.text(example["plan"][attempt_idx])
 
 st.write("## Code")
 st.code(example["code"][attempt_idx])
-st.text(example["code"][attempt_idx])
+#st.text(example["code"][attempt_idx])
 
 
 st.write("## IO")
